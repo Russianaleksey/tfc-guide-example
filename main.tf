e87ccc91b4ba14a3d1db0947820a486800c7b9cc
Mamontov, Alex <aam05@acuitysso.com>
2021-09-15 03:21:57 -0400

    added network security group to config

--- main.tf
+++ main.tf
@@ -3,18 +3,41 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.region
 
   tags = {
-     Environment = "Terraform Getting Started"
-     Team        = "Product Security"
+    Environment = "Terraform Getting Started"
+    Team        = "Product Security"
   }
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name                = "myTFVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = var.region
-    resource_group_name = azurerm_resource_group.rg.name
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "alexNSGtftest"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "ProductSecurity"
+  }
+
 }
--- versions.tf
+++ versions.tf
@@ -1,8 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 2.65"
+    }
+  }
+
+  backend "remote" {
+    organization = "alex-personal"
+    workspaces {
+      name = "alex-testing-workspace"
     }
   }
 
