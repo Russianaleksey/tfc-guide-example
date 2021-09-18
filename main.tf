49f950d67c40879d1995a1bc2bae781d8f959ad7
Mamontov, Alex <aam05@acuitysso.com>
2021-09-18 07:16:03 -0400

    added ssh access for me

--- main.tf
+++ main.tf
@@ -19,13 +19,13 @@ resource "azurerm_network_security_group" "NSG-1" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "AllowRDP"
+    name                       = "AllowSSH"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "3389"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
